13536713958af07df94b331ef58ee79ffadb0048
al Mohamady <al.Mohamady47@gmail.com>
2023-12-30 18:27:42 +0200

    add appointments module

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Hospital Manager",
-
     'summary': "Product for hospitals and patients",
-
     'description': """
           using this module for complete manage your hospital and get good reports 
     """,
-
     'author': "alMohamady",
     'website': "https://www.alMohamady.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'depends': ['base', 'mail'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'views/templates.xml',
         'views/views.xml',
         'views/doctors.xml',
+        'views/appointments.xml',
+        'views/menus.xml',
     ],
 }
 
--- models/models.py
+++ models/models.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class Patients(models.Model):
     _inherit = 'res.partner'
 
     is_patient = fields.Boolean(string="Is Patient")
+    birthdate = fields.Date(string="Birthdate")
+    age = fields.Integer(string="Age")
 
 
 class ResUsers(models.Model):
@@ -14,3 +16,23 @@ class ResUsers(models.Model):
 
     is_doctor = fields.Boolean(string='Is Doctor')
 
+
+class TheAppointments(models.Model):
+    _name = "the.appointments"
+    _description = 'Appointments module'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char(string='Appointment Id', required=True, copy=False, readonly=True,
+                       index=True, default=lambda self: _('New'))
+    patient_id = fields.Many2one('res.partner', string='Patient', required=True,
+                                 domain="[('is_patient', '=', True)]")
+    patient_age = fields.Integer('Age')
+    notes = fields.Text(string='notes')
+    app_date = fields.Datetime(string="Date Time", required=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('the.appointments.sequence') or _('New')
+        result = super(TheAppointments, self).create(vals)
+        return result
